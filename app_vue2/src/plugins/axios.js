0a5f2985f152da66e242670cacd69d352409032a
ariel_braga <ariel.braga@qualirede.com.br>
2023-04-12 18:07:40 -0300

    Adicionado ao axios o token para autorização

--- app_vue2/src/plugins/axios.js
+++ app_vue2/src/plugins/axios.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 import Vue from 'vue';
+import Cookie from '@/service/cookie';
 
-axios.defaults.baseURL = 'http://localhost:8000/api';
+axios.defaults.baseURL = 'http://api.todolist.test/api';
+
+const token = Cookie.getToken();
+
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.headers.authorization = token;
 
 Vue.prototype.$axios = axios;
